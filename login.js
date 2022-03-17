84b580fb2adb43610ba59f2e858899693540f169
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2022-03-17 18:09:59 -0500

    add check of token for user

--- login.js
+++ login.js
@@ -3,11 +3,21 @@
 function login(id, name) {
   if (id === "23454") {
     if (name === "Alex") {
+      const id = checkout(id);
       console.log("This is user correct");
     } else {
       console.log("This not is user");
     }
   }
+
+  return id;
+}
+
+function checkout(token) {
+  if (token) {
+    const id = "213412345234123";
+  }
+  return id;
 }
 
 export default login;
