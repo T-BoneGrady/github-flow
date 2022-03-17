0922bb2adf5bd57da130677121185ef57bd6f9bb
retr-0 <retr-0@pop-os.localdomain>
2022-03-17 16:04:29 -0500

    If user is correct or not

--- login.js
+++ login.js
@@ -1,5 +1,11 @@
 import login from 'lg';
 
-export function login() {
-  conole.log('you is user');
+export function login(id, name) {
+  if (id === "23454") {
+    if (name === "Alex") {
+      console.log("This is user correct");
+    } else {
+      console.log("This not is user");
+    }
+  }
 }
